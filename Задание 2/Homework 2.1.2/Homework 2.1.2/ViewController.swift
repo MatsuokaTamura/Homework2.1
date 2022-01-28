437dad263bb4c2ee881f7a3d87da824c3cb677ff
matsuoka_tamura <sam-anas2@yandex.ru>
2022-01-28 17:44:03 +0300

    Homework 2.1.1 (Задание 1 из 2)

---
+++ Задание 2/Homework 2.1.2/Homework 2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.1.2
+//
+//  Created by Matsuoka Tamura on 28.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

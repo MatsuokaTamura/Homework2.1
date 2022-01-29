251b4a5fea96b572de669b5fffdae3ae877a3747
matsuoka_tamura <sam-anas2@yandex.ru>
2022-01-29 17:01:04 +0300

    Homework 2.1 задание 2

--- Задание 2/Homework 2.1.2/Homework 2.1.2/ViewController.swift
+++ Задание 2/Homework 2.1.2/Homework 2.1.2/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum Light {
+    case red, yellow, green
 }
 
+class ViewController: UIViewController {
+    // Объединяем с main в viewController
+    @IBOutlet var RedLightView: UIView!
+    @IBOutlet var YellowLightView: UIView!
+    @IBOutlet var GreenLightView: UIView!
+    
+    @IBOutlet var StartButton: UIButton!
+    
+    //Задаем параметры яркости цветов
+    private var light = Light.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        //Округляем кнопочку и выключаем все наши квадратики
+        StartButton.layer.cornerRadius = 20
+        RedLightView.alpha = lightOff
+        YellowLightView.alpha = lightOff
+        GreenLightView.alpha = lightOff
+    }
+    
+    // Округляем наши квадратики
+    override func viewWillLayoutSubviews() {
+        RedLightView.layer.cornerRadius = RedLightView.frame.width / 2
+        YellowLightView.layer.cornerRadius = YellowLightView.frame.width / 2
+        GreenLightView.layer.cornerRadius = GreenLightView.frame.width / 2
+    }
+
+    @IBAction func StartButtonAction() {
+        // Чтобы менялось название кнопочки
+        if StartButton.currentTitle == "Start" {
+            StartButton.setTitle("Next", for: .normal)
+        }
+        // Чтобы сменялись цвета
+        switch light {
+        case .red:
+            GreenLightView.alpha = lightOff
+            RedLightView.alpha = lightOn
+            light = .yellow
+            
+        case .yellow:
+            RedLightView.alpha = lightOff
+            YellowLightView.alpha = lightOn
+            light = .green
+            
+        case .green:
+            GreenLightView.alpha = lightOn
+            YellowLightView.alpha = lightOff
+            light = .red
+        }
+    }
+}
